b49cf2c305ca8c26d9dfb76dbe882c31a254b5c2
rv <rajavigneshofficial@gmail.com>
2024-08-14 12:42:43 +0530

    commit 3

--- C42_programs/src/com/tnsif/day2/EncapsulationDemo.java
+++ C42_programs/src/com/tnsif/day2/EncapsulationDemo.java
@@ -1,32 +1,40 @@
 package com.tnsif.day2;
 
 public class EncapsulationDemo {
-	// Team RCB
 	
-			String playerName;
-			int jersyNo;
-			int orderNo; 
+	private static String playerName;
+	private static int orderNo;
+	private static int jersyNo;
 
-	public static void main(String[] args) {
+	//Team RCB
+	
+	
+	//constructor
+	
+	//non-parameterized constructor
+   public EncapsulationDemo() {
+	
+	}
+	
+	
+	//Parameterized constructor
+	public EncapsulationDemo(String playerName,int orderNo, int jersyNo) {
 		
 		
+		this.playerName=playerName;
+		this.orderNo=orderNo;
+		this.jersyNo=jersyNo;
+
 		
 	}
-
+	
+	
 	public String getPlayerName() {
 		return playerName;
 	}
 
 	public void setPlayerName(String playerName) {
 		this.playerName = playerName;
-	}
-
-	public int getJersyNo() {
-		return jersyNo;
-	}
-
-	public void setJersyNo(int jersyNo) {
-		this.jersyNo = jersyNo;
 	}
 
 	public int getOrderNo() {
@@ -36,5 +44,48 @@ public class EncapsulationDemo {
 	public void setOrderNo(int orderNo) {
 		this.orderNo = orderNo;
 	}
+
+	public int getJersyNo() {
+		return jersyNo;
+	}
+
+	public void setJersyNo(int jersyNo) {
+		this.jersyNo = jersyNo;
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "EncapsulationDemo [PlayerName=" + playerName + ", orderNo=" + orderNo + ", jersyNo=" + jersyNo + "]";
+	}
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		EncapsulationDemo obj = new EncapsulationDemo(playerName, jersyNo, orderNo);
+	
+		obj.setPlayerName("Virat kohli");
+		obj.setJersyNo(18);
+		obj.setOrderNo(1);
+		System.out.println(obj);
+	
+	}
+
+	
+	
+	
+	
+	
+	//Access Modifiers or specifiers
+	
+	
+	
+	
+	
+	
+	
 
 }
