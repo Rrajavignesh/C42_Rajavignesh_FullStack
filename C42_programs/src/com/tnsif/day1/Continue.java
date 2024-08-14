5853594ab1ff6dcfb95506aa3dfd38d31a4a3dc5
rv <rajavigneshofficial@gmail.com>
2024-08-14 23:38:29 +0530

    continue

---
+++ C42_programs/src/com/tnsif/day1/Continue.java
@@ -0,0 +1,19 @@
+package com.tnsif.day1;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		//normal iterating process
+		for(int i=3; i<=10; i++) {
+			//check the number is even 
+			//when it's even continue statement is work
+			if(i%2 !=0)
+				continue;
+			//now print only the even number
+			System.out.print(i + " " );
+			//output is 2 4 6 8 10
+		}
+
+	}
+
+}
