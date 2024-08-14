ecd06f403b0ff15089de4e59232e82933b72e6a8
suriya <jayasuriya2824@gmail.com>
2024-08-14 11:06:42 +0530

    new

--- C42_programs/src/com/tnsif/day2/EncapsulationDemo.java
+++ C42_programs/src/com/tnsif/day2/EncapsulationDemo.java
@@ -1,10 +1,40 @@
 package com.tnsif.day2;
 
 public class EncapsulationDemo {
+	// Team RCB
+	
+			String playerName;
+			int jersyNo;
+			int orderNo; 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
+	}
 
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public int getJersyNo() {
+		return jersyNo;
+	}
+
+	public void setJersyNo(int jersyNo) {
+		this.jersyNo = jersyNo;
+	}
+
+	public int getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
 	}
 
 }
