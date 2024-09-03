165d025ef342ab24cd7ed30c5bd8b57f72658470
rv <rajavigneshofficial@gmail.com>
2024-09-03 21:14:02 +0530

    day 8

---
+++ C42_programs/src/com/tnsif/day8/Students.java
@@ -0,0 +1,53 @@
+package com.tnsif.day8;
+
+public class Students {
+
+	//Program to demonstrate Student entity  
+
+
+		private int rollNo;
+		private String name;
+		private float per;
+
+		public int getRollNo() {
+			return rollNo;
+		}
+
+		public void setRollNo(int rollNo) {
+			this.rollNo = rollNo;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public float getPer() {
+			return per;
+		}
+
+		public void setPer(float per) {
+			this.per = per;
+		}
+
+		public Students(int rollNo, String name, float per) {
+			super();
+			this.rollNo = rollNo;
+			this.name = name;
+			this.per = per;
+		}
+
+		public Students() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public String toString() {
+			return "Student [rollNo=" + rollNo + ", name=" + name + ", per=" + per + "]";
+		}
+
+}
